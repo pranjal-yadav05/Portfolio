0dac26f340b97a3a126beed6b524b104f2495431
pranjal-yadav05 <pranjalyadavpy212005@gmail.com>
2025-03-14 17:59:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-      unoptimized: true,
-    },
-    basePath: "/pranjal-yadav05.github.io", // Set this to your GitHub Pages repo name
-    assetPrefix: "/pranjal-yadav05.github.io/",
   };
   
   export default nextConfig;
